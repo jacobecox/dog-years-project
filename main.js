171c150e7eb35e9c938063cf865a077b51c47e36
Jacob <163480591+jacobecox@users.noreply.github.com>
2024-03-14 17:04:49 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+var myAge = 25
+// my current age
+
+var earlyYears = 2
+//early years of a dog's life
+
+earlyYears *= 10.5
+
+var earlyYears = 2 * 10.5
+
+var laterYears = 23
+//early years subtracted from my age
+
+var laterYears = 23 *  4
+//later years multiplied by number of dog years per human year
+
+//console.log(earlyYears, laterYears);
+
+var myAgeInDogYears = earlyYears += laterYears
+//adding early years and later years to get total years
+
+Name = 'Jacob'
+
+// console.log(Name.toLowerCase());
+
+var myName = 'jacob'
+//stating name in lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+// this line plugs the variables into the final answer written in the sentance above
